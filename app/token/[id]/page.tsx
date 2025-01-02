e3512e62924658440f911d92e5af68c670fd842b
messigoalzzz <793148623@qq.com>
2025-01-02 15:59:16 +0800

    fix(简化22):22

--- app/token/[id]/page.tsx
+++ app/token/[id]/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 
-const TokenDetailPage = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
+const TokenDetailPage = () => {
 
   return (
     <div style={{ padding: "20px" }}>
       <h1>Token Details</h1>
-      <p>Token ID: {id}</p>
-      <p>This is the detail page for token with ID: {id}.</p>
+      <p>Token ID: </p>
+      <p>This is the detail page for token with ID:.</p>
     </div>
   );
 };
