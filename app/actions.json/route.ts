31bebaf22227643e7de4ffa02a14ed46449bb61a
messigoalzzz <793148623@qq.com>
2025-01-10 11:13:57 +0800

    添加

--- app/actions.json/route.ts
+++ app/actions.json/route.ts
@@ -5,7 +5,7 @@ export const GET = async () => {
       "rules": [
         {
           "pathPattern": "/**",
-          "apiPath": "/api/actions/start_action"
+          "apiPath": "/api/action/:id"
         }
       ]
   };
