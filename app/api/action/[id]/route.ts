1394aa02fe5a1e213dc87cbc8eef79785a6bfb5e
messigoalzzz <793148623@qq.com>
2025-01-10 13:49:27 +0800

    fix(模拟测试): 处理

--- app/api/action/[id]/route.ts
+++ app/api/action/[id]/route.ts
@@ -1,185 +1,119 @@
-import { ActionPostRequest, ActionPostResponse, createActionHeaders, createPostResponse } from "@solana/actions";
-import { PublicKey, Connection, clusterApiUrl, SystemProgram, Transaction, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { NextResponse } from "next/server";
+import { ActionGetResponse, createActionHeaders } from "@solana/actions";
+
 const headers = createActionHeaders();
-export const GET = async () => {
+
+export const GET = async (
+  req: Request,
+  props: { params: Promise<{ id: string }> }
+) => {
+  const { id } = await props.params;
+
+  const tokenData = {
+    tokenDetails: {
+      createdAt: 1735888715806,
+      updatedAt: 1736475750033,
+      id: 976,
+      token_name: "DogePaw",
+      contract_address: "Hqqcxfg5ti75U4T3Vtry6Xm83A21qNCzaoHSkYiDVt1Z",
+      ticker_symbol: "DPAW",
+      supply: 1000000000,
+      market_cap: 42.5557184869,
+      avaliable_buy: 589813646.493254,
+      sol_balance: 7.01204,
+      token_number: null,
+      token_price: 4.26e-8,
+      description:
+        "DogePaw is a fun and engaging cryptocurrency inspired by the playful nature of dogs. With its unique branding based on a charming dog avatar, DogePaw aims to capture the hearts of pet lovers and meme enthusiasts alike. Join the pack and enjoy the journey with DogePaw, the token that combines the love for dogs with the excitement of crypto.",
+      image_url:
+        "https://ipfs.io/ipfs/QmVw7bUs2G5Ak1aSWPic7WFikdc1goBqfPvihTkNbfG9M3",
+      creator_id: 62,
+      in_progress: 25,
+      votes: 0,
+      vote_down: 0,
+      vote_up: 0,
+      website: "",
+      telegram: "",
+      twitter: "t745628192",
+      creator_wallet_address: "5yJEP8za1fBH7GjZ6XPrmVFMJihDwskTCw9a6uYf2LnN",
+      market_id: "",
+      amm_pool_id: "",
+      stake_pool_id: "",
+      bondingcurve_address: "",
+      metadata_uri:
+        "https://ipfs.io/ipfs/QmVw7bUs2G5Ak1aSWPic7WFikdc1goBqfPvihTkNbfG9M3",
+      on_chain: true,
+      show_in_home: true,
+      launch_by_ai: true,
+      twitter_img_url:
+        "https://ipfs.io/ipfs/QmdD62z5GrmNjuSyxQJHooNYhj96fNyLZsgV9Fs28hB3a6",
+      category: 1,
+      live: {
+        status: 0,
+        platform: 1,
+        live_url: "",
+      },
+      voteStatus: "abstentions",
+      creator: {
+        id: 62,
+        nickname: "5yJEP8...2LnN",
+        profile_photo_url: "https://static.moonpump.me/avatars/9.png",
+      },
+      status: 0,
+    },
+  };
+
+  const tokenInfo = tokenData.tokenDetails;
+
+  const baseHref = new URL(
+    `/api/actions/${id}?on_chain=${tokenInfo.on_chain}&token_name=${tokenInfo.token_name}&ticker_symbol=${tokenInfo.ticker_symbol}&metadata_uri=${tokenInfo.metadata_uri}&contract_address=${tokenInfo.contract_address}`,
+    process.env.NEXTAUTH_URL
+  ).toString();
+
   // 生成动态的响应数据
-  const payload = {
-    title: "ThreeCupsOneBall",
-    icon: "https://www.threecupsoneball.fun/start.jpg",
-    description:
-      "Done be afraid now, take a pick!Guess correctly and win a prize",
-    label: "Play!",
+  const payload: ActionGetResponse = {
+    type: "action",
+    title: `Buy ${tokenInfo.ticker_symbol}`,
+    description: `${tokenInfo.description}`,
+    icon: `${tokenInfo.twitter_img_url || tokenInfo.image_url}`,
+    label: `Buy ${tokenInfo.token_name}`,
     links: {
       actions: [
         {
-          type: "post",
-          label: "Left",
-          href: "https://www.threecupsoneball.fun/api/actions/start_action?path1=path1/correct_action",
+          type: "transaction",
+          label: "0.1 SOL", // button text
+          href: `${baseHref}&amount=${"0.1"}`,
         },
         {
-          type: "post",
-          label: "Middle",
-          href: "https://www.threecupsoneball.fun/api/actions/start_action?path2=path1/wrong_action",
+          type: "transaction",
+          label: "0.5 SOL", // button text
+          href: `${baseHref}&amount=${"0.5"}`,
         },
         {
-          type: "post",
-          label: "Right",
-          href: "https://www.threecupsoneball.fun/api/actions/start_action?path3=path1/wrong_action",
+          type: "transaction",
+          label: "1 SOL", // button text
+          href: `${baseHref}&amount=${"1"}`,
+        },
+        {
+          type: "transaction",
+          label: `Buy ${tokenInfo.ticker_symbol}`, // button text
+          href: `${baseHref}&amount={amount}`, // this href will have a text input
+          parameters: [
+            {
+              name: "amount", // parameter name in the `href` above
+              label: "Enter a custom SOL amount", // placeholder of the text input
+              required: true,
+            },
+          ],
         },
       ],
     },
   };
 
-  // 创建响应对象，并添加 CORS 头
-  const response = NextResponse.json(payload, { headers});
-  // response.headers.set("Access-Control-Allow-Origin", "*");
-  // response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-  // response.headers.set("Access-Control-Allow-Headers", "Content-Type");
-
-  return response;
+  return NextResponse.json(payload, { headers });
 };
 
-// 确保 OPTIONS 方法支持 CORS 预检请求
+// OPTIONS 路由：确保跨域支持
 export const OPTIONS = async () => {
-  const response = new NextResponse(null, { headers });
-  // response.headers.set("Access-Control-Allow-Origin", "*");
-  // response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-  // response.headers.set("Access-Control-Allow-Headers", "Content-Type");
-
-  return response;
+  return new Response(null, { headers });
 };
-
-
-export const POST = async (req: Request) => {
-  try {
-    const requestUrl = new URL(req.url);
-    const { amount, toPubkey } = validatedQueryParams(requestUrl) as {
-      toPubkey: PublicKey;
-      amount: number;
-    };
-
-    console.log("----requestUrl--11", requestUrl);
-    console.log("----amount--22", amount);
-    console.log("----toPubkey--33", toPubkey);
-
-    const body: ActionPostRequest = await req.json();
-
-    // validate the client provided input
-    let account: PublicKey;
-    try {
-      account = new PublicKey(body.account);
-    } catch (err) {
-      console.log("---err", err);
-      return new Response('Invalid "account" provided', {
-        status: 400,
-        headers,
-      });
-    }
-
-    console.log("----account--44", account);
-
-    const connection = new Connection(
-      process.env.SOLANA_RPC! || clusterApiUrl("mainnet-beta")
-    );
-
-    // ensure the receiving account will be rent exempt
-    const minimumBalance = await connection.getMinimumBalanceForRentExemption(
-      0 // note: simple accounts that just store native SOL have `0` bytes of data
-    );
-    if (amount * LAMPORTS_PER_SOL < minimumBalance) {
-      throw `account may not be rent exempt: ${toPubkey.toBase58()}`;
-    }
-
-    // create an instruction to transfer native SOL from one wallet to another
-    const transferSolInstruction = SystemProgram.transfer({
-      fromPubkey: account,
-      toPubkey: toPubkey,
-      lamports: amount * LAMPORTS_PER_SOL,
-    });
-
-    // get the latest blockhash amd block height
-    const { blockhash, lastValidBlockHeight } =
-      await connection.getLatestBlockhash();
-
-    // create a legacy transaction
-    const transaction = new Transaction({
-      feePayer: account,
-      blockhash,
-      lastValidBlockHeight,
-    }).add(transferSolInstruction);
-
-    // versioned transactions are also supported
-    // const transaction = new VersionedTransaction(
-    //   new TransactionMessage({
-    //     payerKey: account,
-    //     recentBlockhash: blockhash,
-    //     instructions: [transferSolInstruction],
-    //   }).compileToV0Message(),
-    //   // note: you can also use `compileToLegacyMessage`
-    // );
-
-    const payload: ActionPostResponse = await createPostResponse({
-      fields: {
-        type: "transaction",
-        transaction,
-        message: `Sent ${amount} SOL to Alice: ${toPubkey.toBase58()}`,
-      },
-      // note: no additional signers are needed
-      // signers: [],
-    });
-
-    return Response.json(payload, {
-      headers,
-    });
-  } catch (err) {
-    console.log(err);
-    const message = "Something went wrong demo";
-    return new Response(
-      JSON.stringify({ message }), // 包装成 JSON 格式
-      {
-        status: 400,
-        headers,
-      }
-    );
-  }
-};
-
-function validatedQueryParams(requestUrl: URL) {
-  let toPubkey: PublicKey = new PublicKey(
-    "FWXHZxDocgchBjADAxSuyPCVhh6fNLT7DUggabAsuz1y"
-  );
-  let amount: number = 0.1;
-
-  if (requestUrl.searchParams.get("to")) {
-    toPubkey = new PublicKey(requestUrl.searchParams.get("to")!);
-  }
-
-  try {
-    if (requestUrl.searchParams.get("amount")) {
-      amount = parseFloat(requestUrl.searchParams.get("amount")!);
-    }
-
-    if (amount <= 0)
-      return new Response("不合法笨蛋", {
-        status: 400,
-        headers,
-      });
-  } catch (err) {
-    console.log("---err", err);
-    //  NextResponse.json(
-    //   { error: "Invalid input query parameter", message: '哈哈哈哈' },
-    //   { status: 400, headers: { "Content-Type": "application/json" } }
-    // );
-
-    return new Response("不合法笨蛋", {
-      status: 400,
-      headers,
-    });
-  }
-
-  return {
-    amount,
-    toPubkey,
-  };
-}
